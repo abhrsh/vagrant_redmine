eebf8674505204796caf808861100d7c336db7e5
安部 浩史 <abc@be.le.to>
2013-04-18 01:42:45 +0900

    subversion追加

--- chef-repo/site-cookbooks/mysql/recipes/default.rb
+++ chef-repo/site-cookbooks/mysql/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: redmine
+# Cookbook Name:: mysql
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
--- provision/exec-all.sh
+++ provision/exec-all.sh
@@ -9,4 +9,5 @@ CHEF_REPO=$ROOT/chef-repo
 
 cd $CHEF_REPO
 berks install --path cookbooks
-chef-solo -c solo.rb -j nodes/project-server.json
+chef-solo -c solo.rb -j nodes/base.json
+#chef-solo -c solo.rb -j nodes/project-server.json
